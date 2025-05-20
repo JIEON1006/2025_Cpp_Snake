b1e2404fe106cac14c0b024d32dccdc67616b979
byeonatt <menggun23@gmail.com>
2025-05-21 00:23:54 +0900

    숫자>문자 매핑
    int 인자를 받아서 char 인자로 return하는 mapping 함수 제작
    블럭 모양 설정 가능; 따로 모양 바꿀 일 없으면 코드 간략하게 통일하겠습니다.
    숫자에 따른 블럭 유형은 임의로 적어뒀는데 추후 상의 후 수정합시당
    
    map.cpp는 수정하지 않음: 잘못 건드렸다가 도로 되돌려놔서 수정 기록이 남았어요

--- include/constant.h
+++ include/constant.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <iostream>
 #include <string>
+#include <ncurses.h>
 using namespace std;
 
-string  test = "constant.h 테스트";
+char mapping(int num){
+    switch(num) {
+        case 0: return '■';     // 0 : snake가 자유롭게 이동가능한 공백 블럭
+        case 1: return '■';     // 1 : 그냥 벽
+        case 2: return '■';     // 2 : 모서리 벽
+        case 3: return '■';     // 3 : snake의 머리
+        case 4: return '■';     // 4 : snake의 몸통
+        case 5: return '■';     // 5 : gate 블럭
+        case 6: return '■';     // 6 : plus 아이템
+        case 7: return '■';     // 7 : minus 아이템 
+        // ... 기타 블럭 추가 가능 
+    }
+}
